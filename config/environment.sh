601c33586c9845a562101561b56ad390bd6a6319
Rotsten <kirsirotsten@gmail.com>
2015-10-29 15:37:47 +0200

    Sovellus alustettu ja versionhallinta otettu käyttöön

---
+++ config/environment.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/bash
+
+# korvaa tämä omalla käyttäjätunnuksellasi
+USERNAME="rotsten"
+# korvaa tämä haluamallasi kansion nimellä
+PROJECT_FOLDER="Varasto"
+# sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
