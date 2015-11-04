b6f9d999dea80105c76b388c58a5d95360155009
Rotsten <kirsirotsten@gmail.com>
2015-11-04 14:22:19 +0200

    Testataan (dymmy-)kirjautumista

--- app/views/handle_reg.php
+++ app/views/handle_reg.php
@@ -4,33 +4,38 @@ Tietokantasovellus harjoitustyö, 2015
 Kirsi Rotstén 
  */
 
-<?php 
 /* This script receives two values from Home.html:
  * These are: käyttäjätunnus & salasana 
- * Returns the text, whether field is empty or not */
+ * Returns the text, whether fields are empty or not.
+ * Later add also other mechanisms to check the validity of the inputs... 
+ */
+ 
+<?php
 
-// Success-flag setting
-$okay = TRUE;
+function handle_reg (){
 
-// Tsekkaa antoiko käyttäjätunnuksen:
-if (empty($_POST['user_id'])) {
-	print '<p class="error">Anna käyttäjätunnus.</p>';
-	$okay = FALSE;
+    // Success-flag setting
+    $okay = TRUE;
+
+    // Tsekkaa antoiko käyttäjä käyttäjätunnuksen:
+    if (empty($_POST['user_id'])) {
+            print '<p class="error">Anna käyttäjätunnus.</p>';
+            $okay = FALSE;
+    }
+
+    // Tsekkaa antoiko käyttäjä salasanan:
+    if (empty($_POST['password'])) {
+            print '<p class="error">Anna salasana.</p>';
+            $okay = FALSE;
+    }
+    /* Pitäisi tarkastella Käyttäjä-taulun tiedoista myös, löytyykö käyttäjä */
+
+    if ($okay) {
+        // print '<p>Onnistunut kirjautuminen.</p>';
+        return TRUE;
+    }
+    else 
+        //print '<p>Yritä uudelleen.</p>';
+        return FALSE;
 }
-
-// Tsekkaa antoiko salasanan:
-if (empty($_POST['password'])) {
-	print '<p class="error">Anna salasana.</p>';
-	$okay = FALSE;
-}
-
-/* Pitäisi tarkastella Käyttäjä-taulun tiedoista myös, löytyykö käyttäjä */
-
-if ($okay) {
-    // print '<p>Onnistunut kirjautuminen.</p>';
-    return TRUE;
-}
-else 
-    //print '<p>Yritä uudelleen.</p>';
-    return FALSE;
-?>
+    ?>
