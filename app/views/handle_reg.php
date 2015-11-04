f3bd83c2130ba30e3df52c1f4ba3999874a3991e
Rotsten <kirsirotsten@gmail.com>
2015-11-04 12:38:12 +0200

    Testataan (dymmy-)kirjautumista

--- app/views/handle_reg.php
+++ app/views/handle_reg.php
@@ -10,6 +10,18 @@ Kirsi Rotstén
 // Success-flag setting
 $okay = TRUE;
 
+// Validate the email address:
+if (empty($_POST['user_id'])) {
+	print '<p class="error">Anna käyttäjätunnus.</p>';
+	$okay = FALSE;
+}
+
+// Validate the password:
+if (empty($_POST['password'])) {
+	print '<p class="error">Anna salasana.</p>';
+	$okay = FALSE;
+}
+
 // If there were no errors, print a success message:
 if ($okay) {
     print '<p>Onnistunut kirjautuminen.</p>';
