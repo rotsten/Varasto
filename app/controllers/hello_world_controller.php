287150a892f607ba0785952d7ba3b1d56d56294e
Rotsten <kirsirotsten@gmail.com>
2015-11-05 14:50:18 +0200

    Korjauksia

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -8,33 +8,35 @@
       // echo 'Tämä on etusivu!';
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      // echo 'Hello World!';
-      // View::make('HelloWorld.html');
-    }
-    
     public static function login(){
        View::make('Suunnitelmat/Aloitussivu.html');
+    }
+    
+    public static function tuote_list(){
+       echo 'Hello World!';
+       View::make('Suunnitelmat/Tuotteidenlistaus.html');
+    }
+    
+    public static function tuote_show(){
+      echo 'Hello World!';
+      View::make('Suunnitelmat/Tuotesivu.html');
+    }
+
+    public static function varasto_list(){
+       View::make('Suunnitelmat/Varastonlistaus.html');
+    }
+    
+    public static function varasto_show(){
+      View::make('Suunnitelmat/Varastotilanteenmuutos.html');
     }
     
     public static function kayttaja_list(){
        View::make('Suunnitelmat/Käyttäjienlistaus.html');
     }
     
-    public static function tuote_list(){
-       View::make('Suunnitelmat/Tuotelistaus.html');
-    }
-    
-    public static function varasto_list(){
-       View::make('Suunnitelmat/Varastonlistaus.html');
-    }
-
-    public static function tuote_show(){
-      View::make('Suunnitelmat/Tuotesivu.html');
-    }
-    
-    public static function varasto_show(){
-      View::make('Suunnitelmat/Varastotilanteenmuutos.html');
+    public static function sandbox(){
+      // Testaa koodiasi täällä
+      // echo 'Hello World!';
+      // View::make('HelloWorld.html');
     }
   }
