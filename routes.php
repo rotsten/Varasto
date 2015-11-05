089a2205c190d4166cd8535f29e220c674aec881
Rotsten <kirsirotsten@gmail.com>
2015-11-05 14:43:23 +0200

    Korjauksia

--- routes.php
+++ routes.php
@@ -4,16 +4,32 @@
     HelloWorldController::index();
   });
 
+  $routes->get('Suunnitelmat/Login', function() {
+    HelloWorldController::login();
+  });
+  
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
   
   $routes->get('Suunnitelmat/Tuotesivu', function() {
-    HelloWorldController::tuote_list();
+    HelloWorldController::tuote_show();
   });
   
   $routes->get('Suunnitelmat/Tuotelistaus', function() {
-    HelloWorldController::tuote_show();
+    HelloWorldController::tuote_list();
+  });
+  
+    $routes->get('Suunnitelmat/Kayttajienlistaus', function() {
+    HelloWorldController::kayttaja_list();
+  });
+
+  $routes->get('Suunnitelmat/Varastotilanteenmuutos', function() {
+    HelloWorldController::varasto_show();
+  });
+  
+  $routes->get('Suunnitelmat/Varastolistaus', function() {
+    HelloWorldController::varasto_list();
   });
 
   $routes->get('Suunnitelmat/Aloitussivu', function() {
