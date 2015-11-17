d983780fef07fea3d0f06c343d43b690ff000a36
Rotsten <kirsirotsten@gmail.com>
2015-11-17 23:56:21 +0200

    Muutoksia kirjautumiseen

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -9,7 +9,8 @@
     public static function sandbox(){
       echo 'Hip-hei, täällä ollaan!';
       //TuoteController::find_tuotteennimi('Lakua');
-      TuoteController::find_tuote('9789522910325');
+      //TuoteController::find_tuote('9789522910325');
+      TuoteController::tuote_edit('9789522641632');
       //TuoteController::find_tuote_with_tuote_id('9789522910325');
 
     }
