6b41c6250fdaf2c92d1c8487cce744f08b0b3a10
Rotsten <kirsirotsten@gmail.com>
2015-11-17 13:58:36 +0200

    Korjauksia hakutuloksen esittelyyn

--- config/routes.php
+++ config/routes.php
@@ -90,18 +90,20 @@
   });
   
   // Tuotteen listaus käyttää tätä
-  /*
+  
   $routes->get('/Tuote/Tuotesivu/{{tuote_id}}', function($tuote_id) {
     //TuoteController::find_tuote_with_tuote_id($tuote_id);
     TuoteController::tuote_show($tuote_id);
   }); 
-   */   
+    
   
+  /*
   // Tuotteen haku (ja listaus) käyttää tätä
   $routes->get('/Tuote/Tuotesivu/:Tuote_id', function($tuote_id) {
     //TuoteController::find_tuote_with_tuote_id($tuote_id);
     TuoteController::tuote_show($tuote_id);
   }); 
+  */
   
   // Listaus käyttää tätä
   $routes->get('/Tuotesivu/:Tuote_id', function($tuote_id) {
