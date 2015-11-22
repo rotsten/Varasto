aff093f5479368c51303d024e71f215852e4f098
Rotsten <kirsirotsten@gmail.com>
2015-11-23 01:43:23 +0200

    Muutoksia poistotoimintoihin

--- app/controllers/TuoteController.php
+++ app/controllers/TuoteController.php
@@ -281,7 +281,7 @@ class TuoteController extends BaseController{
     
     // Käyttäjä näkee listauksesta, että tuote on poistunut      
     $Tuotteet = Tuote::all();
-    View::make('Tuote/Poista.html{{Tuote.tuote_id}}', array('Tuotteet' => $Tuotteet)); 
+    View::make('Tuote/Poista.html{{poistettava_tuote.tuote_id}}', array('Tuotteet' => $Tuotteet)); 
     
     //Redirect::to('/Tuote', array());
     //Redirect::to('/Tuote/Poista', array('Tuotteet' => $Tuotteet));   
