d3cfcab68c944018f1cf933dcca603cde2d2aa5d
Rotsten <kirsirotsten@gmail.com>
2015-11-23 23:21:06 +0200

    Muutoksia tietokantadatoihin

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,32 +1,5 @@
---INSERT INTO KAYTTAJA (Kayttajatunnus, Salasana, Etunimi, Sukunimi, Kayttooikeudet)
---VALUES ('Taneli', 'tuikkurlz', 'Taina-Liisa', 'Matikainen', 'true');
-
---INSERT INTO KAYTTAJA (Kayttajatunnus, Salasana, Etunimi, Sukunimi, Kayttooikeudet)
---VALUES ('Antti', 'the_boss', 'Antti', ' ', 'true');
-
---INSERT INTO KAYTTAJA (Kayttajatunnus, Salasana, Etunimi, Sukunimi, Kayttooikeudet)
---VALUES ('Salmi_1', 'mies74', 'Jarkko', 'Salminen', 'false');
-
 INSERT INTO KAYTTAJA (Kayttajatunnus, Salasana, Etunimi, Sukunimi, Kayttooikeudet)
 VALUES ('Kylli', 'rotta10', 'Kyllikki', 'Kymäräinen', 'true');
- 
---INSERT INTO TUOTE (Tuote_ID, Tuotteen_nimi, Kuvaus, Valmistaja, Lukumaara, history_date)
---VALUES ('9789522910325', 'Big data & pilvipalvelut', 'Immo Salo: Big data & pilvipalvelut', 'Docendo', '2015-05-11 20:45');
-
---INSERT INTO TUOTE (Tuote_ID, Tuotteen_nimi, Kuvaus, Valmistaja, Lukumaara, history_date)
---VALUES ('9781292061351', 'W. Stallings: Operating Systems, Internals and design principles', 'W. Stallings: Operating Systems, Internals and design principles, oppikirja käyttöjärjestelmistä', 'Pearson', '2015-05-11 21:09');
-
---INSERT INTO TUOTE (Tuote_ID, Tuotteen_nimi, Kuvaus, Valmistaja, Lukumaara, history_date)
---VALUES ('9789511174684', 'Suomen lasten kalevala', 'Kuvitettu teos', 'Otava', '2015-05-11 21:11');
-
---INSERT INTO TUOTE (Tuote_ID, Tuotteen_nimi, Kuvaus, Valmistaja, Lukumaara, history_date)
---VALUES ('9789513233259', 'Aku Ankan taskukirja nro 390 "Outolintu"', 'Aku Ankan taskukirja vuodelta 2002', 'Sanoma Magazines', '2015-05-11 20:45');
-
---INSERT INTO TUOTE (Tuote_ID, Tuotteen_nimi, Kuvaus, Valmistaja, Lukumaara, history_date)
---VALUES ('9789522641632', 'Ha-Joon Chang "23 tosiasiaa kapitalismista"', 'Ha-Joon Chang "23 tosiasiaa kapitalismista", Kansainvälinen bestselleri', 'Into Kustannus Oy', '2015-05-11 21:09');
-
---INSERT INTO TUOTE (Tuote_ID, Tuotteen_nimi, Kuvaus, Valmistaja, Lukumaara, history_date)
---VALUES ('6430032616506', 'A4 Avolehtiö', '7x7 mm ruudullinen avolehtiö kokoa A4', 'Wulff Oy Ab', '2015-05-11 21:11');
 
 INSERT INTO TUOTE (Tuote_ID, Tuotteen_nimi, Kuvaus, Valmistaja, Lukumaara, history_date)
 VALUES ('5050582340112', 'Johnny Guitar	Universal', 'Klassikkolänkkäri, formaatti: DVD', '2015-05-11 21:11');
@@ -46,17 +19,8 @@ VALUES ('9789523001084', 'James Church', 'Atena', 'James Church on pseudonyymi, 
 INSERT INTO TUOTE (Tuote_ID, Tuotteen_nimi, Kuvaus, Valmistaja, Lukumaara, history_date)
 VALUES ('9512009110', 'Mauri Sariola: "Susikosken ajojahti"', 'Gummerus', 'Poliisiromaani vuodelta 1974', '2015-05-11 21:11'); 
 
---INSERT INTO TUOTE (Tuote_ID, Tuotteen_nimi, Kuvaus, Valmistaja, Lukumaara, history_date)
---VALUES ('2057030102660', 'Lakua', 'Porvoon herkku', 'Porvoon musta laku, 250 g annospussi', '2015-05-11 21:11'); 
-
---INSERT INTO TUOTE (Tuote_ID, Tuotteen_nimi, Kuvaus, Valmistaja, Lukumaara, history_date)
---VALUES ('9789511174684', 'Suomen lasten kalevala', 'Otava', 'Suomen lasten kalevala kuvitettu teos', '2015-05-11 21:11'); 
-
 INSERT INTO TUOTE (Tuote_ID, Tuotteen_nimi, Kuvaus, Valmistaja, Lukumaara, history_date)
 VALUES ('9781292025407', 'Introduction to Computer Security', 'Pearson', 'Kirjoittajat: Michael Goodrich & Robert Tamassia. Hyvin yleisesti käytetty oppikirja.', '2015-05-11 21:11'); 
-
---INSERT INTO TUOTE (Tuote_ID, Tuotteen_nimi, Kuvaus, Valmistaja, Lukumaara, history_date)
---VALUES ('9789522205087', 'Auta lastasi matematiikassa', 'readme.fi', 'Carol Vorderman: "Auta lastasi matematiikassa läpi koko peruskoulun ja lukion", oppikirja', '2015-05-11 21:11'); 
 
 INSERT INTO TUOTE (Tuote_ID, Tuotteen_nimi, Kuvaus, Valmistaja, Lukumaara, history_date)
 VALUES ('9789511163602', 'Peter von Bagh: "Elämää suuremmat elokuvat"', 'Otava', 'Peter von Bagh: "Elämää suuremmat elokuvat", 511-sivuinen järkälemäinen analyysi maailman parhaista elokuvista', '2015-05-11 21:11'); 
