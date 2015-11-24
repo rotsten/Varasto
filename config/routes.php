5dc413d80a9418f25646cb9aeddf660118f71f8d
Rotsten <kirsirotsten@gmail.com>
2015-11-25 01:02:18 +0200

    Korjauksia uuden käyttäjän lisäämiseen

--- config/routes.php
+++ config/routes.php
@@ -25,7 +25,7 @@
 
   // Tuotteen lisäyslomakkeen näyttäminen
   $routes->get('/Kayttaja/LisaaKayttaja', function(){
-    TuoteController::kayttaja_lisaa_show();
+    KayttajaController::kayttaja_lisaa_show();
   });
   
   // Uuden käyttäjän lisääminen
