7b147f2963867c532545ff5c6e2a8d7e0e103b04
Rotsten <kirsirotsten@gmail.com>
2015-11-25 12:59:12 +0200

    Jatkokehitystä varastotilanteen listaamiseen

--- app/models/Varasto.php
+++ app/models/Varasto.php
@@ -12,7 +12,6 @@
  * @author rotsten
  */
 class Varasto extends BaseModel{
-    //put your code here
     
    // attribuutit
   public $tuote_id, $lukumaara, $history_kuka_inventoi;
@@ -42,8 +41,10 @@ class Varasto extends BaseModel{
     // Käydään kyselyn tuottamat rivit läpi
     foreach($rows as $row){
 
-      $varastotilanne[] = new Varasto(array(
+      // Kysymyksessä ei ole enää Varasto-olio, vaan Varastotilanne -olio
+      $varastotilanne[] = new Varastotilanne(array(
         'tuote_id' => $row['tuote_id'],
+        'tuotteen_nimi' => $row['tuotteen_nimi'],
         'lukumaara' => $row['lukumaara'],  
         'history_kuka_inventoi' => $row['history_kuka_inventoi']
       ));
---
+++ app/models/Varastotilanne.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Varastotilanne
+ *
+ * @author rotsten
+ */
+class Varastotilanne extends Varasto{
+    
+  // attribuutit
+  public $tuotteen_nimi;
+  
+  // konstruktori
+  public function __construct ($varaston_tiedot, $tuotteen_nimi){
+      
+      parent::__construct($varaston_tiedot, $tuotteen_nimi);
+      $this->tuote_id = $varaston_tiedot['tuote_id'];
+      $this->tuotteen_nimi = $tuotteen_nimi;
+      $this->lukumaara = $varaston_tiedot['lukumaara'];
+      $this->history_kuka_inventoi = $varaston_tiedot['history_kuka_inventoi'];
+  }
+  
+}
