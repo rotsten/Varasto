45d5b75d43cd35a9cd38efbc9778a58a19177bec
Rotsten <kirsirotsten@gmail.com>
2015-12-06 22:03:47 +0200

    Lisayksia: muuta Varasto-tuote

--- app/models/VarastoTuote.php
+++ app/models/VarastoTuote.php
@@ -111,7 +111,7 @@ class VarastoTuote extends BaseModel{
     $query = DB::connection()->prepare('SELECT * FROM varasto_tuote 
                                         LEFT JOIN tuote
                                         ON varasto_tuote.tuote_id = tuote.tuote_id
-                                        WHERE varasto_id = :varasto_id;');
+                                        WHERE varasto_id =:varasto_id;');
     // Suoritetaan kysely
     $query->execute(array('varasto_id' => $varasto_id));
     
